3c671e8bd1c61c80c29e92565c5159cd1b7d383c
GooseMagnet <ilankarasik@gmail.com>
2021-03-26 23:33:51 -0400

    Finished Centralized Logging and Distributed Tracing

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,12 @@
+
+D
+frontend/src/App.css,8/a/8a75d7beb1451b6fe9282486fbd897a9a8156b7f
+`
+0frontend/src/components/LeaderBoardComponent.jsx,f/9/f9d2db2231dfe6bc8e9027a4f08d7a7495e25513
+V
+&frontend/src/services/GameApiClient.ts,2/c/2c975f33fc95853808a239217bf1a38f173c924f
+\
+,frontend/src/services/ChallengesApiClient.ts,5/d/5d2b4ae2179d9bfa7a9ccee92e38d369e22dc2d2
+:
+
+.gitignore,a/5/a5cc2925ca8258af241be7e5b0381edf30266302
--- multiplication/src/main/java/microservices/book/multiplication/user/UserController.java
+++ multiplication/src/main/java/microservices/book/multiplication/user/UserController.java
@@ -2,6 +2,7 @@ package microservices.book.multiplication.user;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,7 @@ public class UserController {
 
     @GetMapping("/{idList}")
     public List<User> getUsersByIdList(@PathVariable final List<Long> idList) {
+        log.info("Resolving aliases for users {}", idList);
         return userRepository.findAllByIdIn(idList);
     }
 }
